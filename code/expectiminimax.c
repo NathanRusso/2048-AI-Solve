ad4a481cdf6dbb5e9bbde2b8686c87fcfd49f22c
NathanRusso <nathandanielrusso@gmail.com>
2026-01-15 19:50:06 -0500

    I added header files and constants.

--- code/expectiminimax.c
+++ code/expectiminimax.c
@@ -2,4 +2,12 @@
  * expectiminimax.c - A C, as opposed to Python, version of expectiminimax
  */
 
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#define MAX_BOARD_DIMENSION 4
+#define BLANK_TILE 0
+#define TILE_2_CHANCE 0.9
+#define TILE_4_CHANCE 0.1
