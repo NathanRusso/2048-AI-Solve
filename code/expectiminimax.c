0de2c472b716b0ffe746987e8769e76c8397e9ac
NathanRusso <nathandanielrusso@gmail.com>
2026-01-15 20:38:50 -0500

    I added necessary variables and the primary function.

--- code/expectiminimax.c
+++ code/expectiminimax.c
@@ -11,3 +11,32 @@
 #define BLANK_TILE 0
 #define TILE_2_CHANCE 0.9
 #define TILE_4_CHANCE 0.1
+
+typedef enum {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+} Direction; // NOTE: STARTS AT 0, NOT 1!!!!!!!!!!!!!
+
+const int SNAKE_HEURISTIC_3[4][4] = {
+    {4096, 2048, 1024, 512},
+    {64, 128, 256, 512},
+    {64, 32, 16, 8},
+    {1, 2, 4, 8}
+};
+
+int DEPTH = 5; // The defalt depth for Expectiminimax
+
+/**
+ * This returns the "best" direction to shift the tiles in the given board.
+ * 
+ * @param depth The search depth of the AI solver/search.
+ * 
+ * @return The best direction to move: 1: UP, 2: DOWN, 3: LEFT, 4: RIGHT
+ */
+int getNextDirection(int depth) {
+    DEPTH = depth;
+    Direction best_direction = UP;
+    return best_direction + 1;
+}
